63a7500c8cb1f86431a98c1635e4d220465f6640
souhail <souhailkhalmadani11@gmail.com>
2025-03-27 12:40:01 +0100

    Successfully initialized the webdriver for rekrute.com

--- Data_extraction/Rekrute.py
+++ Data_extraction/Rekrute.py
@@ -5,18 +5,22 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import os
+import time
 
-current_path = os.
+current_path = os.path.abspath(__file__)
+current_dir= os.path.dirname(current_path)
 
-def init_driver(executable_path=r'C:\Users\hp\Documents\Code projects\Stage 2024\Web scrapping\chromedriver\win64-125.0.6422.142\chromedriver-win64\chromedriver.exe'):
+
+def init_driver(executable_path=os.path.dirname(current_dir) + "\chromedriver-win64\chromedriver.exe"):
     # Creation et configuration du Driver, pour pointer sur le driver changez le chemin executable_path 
+    print("The executable path is: ", executable_path)
     service = Service(executable_path)
     chrome_options = Options()
    
     driver = webdriver.Chrome(options=chrome_options, service=service)
     driver.implicitly_wait(2)  # Time before the program exits in case of exception in seconds, will not wait if the program runs normally
     
-    
-    driver.execute_cdp_cmd('Network.setBlockedURLs', {"urls": ["*.jpg", "*.jpeg", "*.png", "*.gif", "*.webp", "*.bmp"]})
-    driver.execute_cdp_cmd('Network.enable', {})
     return driver
+driver=init_driver()
+driver.get("https://www.rekrute.com")
+time.sleep(10)

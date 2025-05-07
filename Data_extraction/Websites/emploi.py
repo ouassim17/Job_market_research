918b21898e24f634f2e944ef641e4cc891acbc8f
souhail <souhailkhalmadani11@gmail.com>
2025-05-07 14:12:33 +0100

    Improved project folder structure

--- Data_extraction/Websites/emploi.py
+++ Data_extraction/Websites/emploi.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
-from selenium_init import init_driver, setup_logger,save_json
+from Data_extraction.Websites.selenium_init import init_driver, setup_logger,save_json
 logger=setup_logger("emploi.log")
 
 # Chargement des offres déjà collectées 

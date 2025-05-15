99648812c4b3065681b57702b6bc223565a2a6f7
souhail <souhailkhalmadani11@gmail.com>
2025-05-15 14:20:02 +0100

    added some file type and directories to .dockerignore, created two separate dockercompose files for dev and prod, changed name of dckerfile to Dockerfile.celery, added some tasks to tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,8 +1,18 @@
 from celery import Celery
 
-app = Celery("tasks", broker="redis://localhost:6379")
+app = Celery("tasks", broker="redis://localhost:6379", backend="redis://localhost:6379")
 
 
-@app.task
+@app.task(name="tasks.add")
 def add(x, y):
     return x + y
+
+
+@app.task(name="tasks.mult")
+def mult(x, y):
+    return x * y
+
+
+@app.task(name="tasks.sub")
+def sub(x, y):
+    return x - y

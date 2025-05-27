2f16646b7e46cb1afe16009c171d427627d84af4
souhail <souhailkhalmadani11@gmail.com>
2025-05-27 09:46:29 +0100

    added all web scraping tasks to celery_app.tasks.py

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -1,25 +1,46 @@
-from celery.utils.log import get_task_logger
-
 from celery_app import app
-from data_extraction.Websites import Rekrute
-
-logger = get_task_logger(__name__)
-
-
-@app.task(
-    name="add",
-)
-def add(x, y):
-    logger.info("Adding {0} + {1}".format(x, y))
-    return x + y
+from data_extraction.Websites import MarocAnn, Rekrute, bayt, emploi
 
 
 @app.task(
     name="rekrute",
 )
-def rekrute():
+def rekrute_task():
     try:
         print("Appel du script rekrute")
         return Rekrute.main()
+    except Exception:
+        print("Exception lors de l'execution")
+
+
+@app.task(
+    name="bayt",
+)
+def bayt_task():
+    try:
+        print("Appel du script bayt")
+        return bayt.main()
+    except Exception:
+        print("Exception lors de l'execution")
+
+
+@app.task(
+    name="Marocannonce",
+)
+def marocann_task():
+    try:
+        print("Appel du script maroc annonces")
+        return MarocAnn.main()
+    except Exception:
+        print("Exception lors de l'execution")
+
+
+@app.task(
+    name="emploi",
+)
+def emploi_task():
+    try:
+        print("Appel du script emploi")
+        return emploi.main()
     except Exception:
         print("Exception lors de l'execution")
